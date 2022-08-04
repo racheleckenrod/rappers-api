e7fcfe17e1b6e481bf1ff65355e18d8bf1ccd9f5
racheleckenrod <rachel@racheleckenrod.com>
2022-08-03 22:35:54 -0500

    update html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert('working')
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://take-two-rappers-api.herokuapp.com/api/${rapperName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.birthName
+    }catch(error){
+        console.log(error)
+    }
+}
